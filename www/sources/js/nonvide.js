84ecc249c74415ab24cbd7c87481781fa4c8e35f
LaetitiaZaleski <laetitia.zaleski@gmail.com>
2019-11-27 15:10:48 +0100

    recherche un par un ok

--- www/sources/js/nonvide.js
+++ www/sources/js/nonvide.js
@@ -42,6 +42,13 @@ async function getPrefs() {
             OuvMin : 0
     };
 
+        const importancesVal = {
+            tImp : 1000.0,
+            imp : 100.0,
+            neutre : 10.0,
+            pImp : 0.0
+        };
+
 
         let className = "";
         // console.log(localStorage.getItem("classId"));
@@ -110,7 +117,7 @@ async function getPrefs() {
                         distVal: obj[i].preference.value_tour_min - minMax.TMin,
                         importance: localStorage.getItem("ImportanceTourMin"),
                         signe: 1,
-                        pas: obj[i].preference.value_tour_min - minMax.TMin,
+                        pas: 0.0,
                         table : [0]
                     };
                     const TourMax = {
@@ -166,86 +173,226 @@ async function getPrefs() {
                 }
             }
         }
+        for (var i = 0; i < Prefs.length; i++) {
+            switch (Prefs[i].importance) {
+                case 'très important':
+                case 'très importante':
+                    Prefs[i].pas = Prefs[i].distVal / parseFloat(importancesVal.tImp);
+                    Prefs[i].importance = parseInt(importancesVal.tImp);
+                    Prefs[i].table = [0, 0, parseInt(importancesVal.tImp)];
+                    console.log(Prefs[i].table);
+                    break;
+                case 'important':
+                case 'importante':
+                    Prefs[i].pas = Prefs[i].distVal / parseFloat(importancesVal.imp);
+                    Prefs[i].importance = parseInt(importancesVal.imp);
+                    Prefs[i].table = [0, 0, parseInt(importancesVal.imp)];
+                    break;
 
-        finalPrefs = rechercheNonVide(Prefs, minMax)
+                case 'neutre':
+                    Prefs[i].pas = Prefs[i].distVal / parseFloat(importancesVal.neutre);
+                    Prefs[i].importance = parseInt(importancesVal.neutre);
+                    Prefs[i].table = [0, 0, parseInt(importancesVal.neutre)];
+                    break;
+                default :
+                    Prefs[i].importance = parseFloat(importancesVal.pImp);
+                    Prefs[i].table = [0, 0, parseInt(importancesVal.pImp)];
+                    Prefs[i].pas = 0;
+                    break;
+            }
+        }
+        for (var j =0; j<Prefs.length; j++){
+        }
+
+        finalPrefs = [];
+        PrefsInit0 = JSON.parse(JSON.stringify(Prefs));
+        PrefsInit1 = JSON.parse(JSON.stringify(Prefs)); // copie du tableau
+        PrefsInit2 = JSON.parse(JSON.stringify(Prefs));
+        console.log(Prefs);
+        console.log(PrefsInit1);
+        console.log(PrefsInit2);
+
+        console.log("recherche 1");
+        let r1 = rechercheDiagonale(PrefsInit0, minMax); // OK tout seul
+
+
+        console.log(Prefs);
+        console.log(PrefsInit1);
+        console.log(PrefsInit2);
+
+        // jusqu'ici tout va bien
+
+        console.log("recherche 2");
+        let r2 =rechercheUnParUn(PrefsInit1, minMax); // OK tout seul
+        console.log(r2.data);
+        console.log(r1.data);
+        finalPrefs = [r1.data, r2.data];
+
+        console.log("Final prefs :");
+
+        for(k = 0; k<finalPrefs.length; k++){
+            console.log("k :");
+            console.log(k);
+            console.log(finalPrefs[k]);
+        }
+
+
+
+
+
+     /*   console.log("recherche 3");
+
+        let priorite = recherchePriorite(PrefsInit2,minMax);
+
+        for(var k =0; k <priorite.length; i++){
+            finalPrefs.push(priorite[i]);
+        }
+
+        for(k = 0; k<finalPrefs.length; k++){
+            console.log(finalPrefs[k].data)
+        }*/
 
     });
 
 }
+function removeDomine(data){ // enlève les éléments dominés d'un ptableau de datas
+    for( var i = 0; i<data.length; i++){
+        d = JSON.parse(data[i]);
+        for( var j = 0; j<data.length; j++){
+            dcompare = JSON.parse(data[i]);
+            if(d.value_ani_min <= dcompare.value_ani_min && d.value_tour_min){
 
-function rechercheNonVide(Prefs, minMax, importab =[0.0,10.0,100.0,1000.0], nbEtapes=100){
+            }
+        }
 
-    prefsInit = [...Prefs];
-    newPrefs = dichotomie(Prefs, minMax,importab, nbEtapes);
-    console.log(newPrefs);
+    }
+
+}
+
+/*
+"value_ani_min": Math.ceil(AniMin.table[1]*AniMin.signe*AniMin.pas + minMax.AMin),
+            "value_ani_max":  Math.floor(AniMax.table[1]*AniMax.signe*AniMax.pas + minMax.AMax),
+            "value_tour_min": Math.ceil(TourMin.table[1]*TourMin.signe*TourMin.pas + minMax.TMin),
+            "value_tour_max": Math.floor(TourMax.table[1]*TourMax.signe*TourMax.pas + minMax.TMax),
+            "value_cap_min": Math.ceil(CapMin.table[1]*CapMin.signe*CapMin.pas + minMax.CMin),
+            "value_cap_max": Math.floor(CapMax.table[1]*CapMax.signe*CapMax.pas + minMax.CMax),
+            "value_env_min": Math.ceil(EnvMin.table[1]*EnvMin.signe*EnvMin.pas + minMax.EnvMin),
+            "value_env_max": Math.floor(EnvMax.table[1]*EnvMax.signe*EnvMax.pas + minMax.EnvMax),
+            "value_ouv_min": Math.ceil(OuvMin.table[1]*OuvMin.signe*OuvMin.pas + minMax.OuvMin),
+            "value_ouv_max":Math.floor(OuvMax.table[1]*OuvMax.signe*OuvMax.pas + minMax.OuvMax)
+*/
+
+
+// renvoie un tableau de noyaux plus proches non vides e
+function rechercheDiagonale(Prefs, minMax, importab =[0.0,10.0,100.0,1000.0], nbEtapes=100){
+
+    prefsInit = JSON.parse(JSON.stringify(Prefs));
+    res = dichotomie(Prefs, minMax,importab, nbEtapes);
 
     /* Rafinement */
-    console.log(importab)
-    finalPrefs = rafinement(newPrefs,prefsInit, minMax,importab,nbEtapes);
+ //   console.log(importab);
+
+    finalPrefs = rafinement(res.pref,prefsInit, minMax,importab,nbEtapes);
 
     return finalPrefs
 
 }
 
+function rechercheUnParUn(Prefs,minMax,importab =[0.0,10.0,100.0,1000.0], nbEtapes=100) { //on se rapproche d'abord des plus important plutot que de tout faire en même temps
+
+    var newPrefs = {
+        pref : JSON.parse(JSON.stringify(Prefs)),
+        data : []
+    };
+
+    console.log(newPrefs.pref);
+    prefsInit = JSON.parse(JSON.stringify(Prefs));
+
+
+    for (var i=0; i<newPrefs.pref.length; i++) {
+        // newImportab = new Array(importab.length).fill(0.0);
+        if (newPrefs.pref[i].importance < importab[importab.length - 1]) { // si on est moins important on s'en occupe pas pour l'instant
+            newPrefs.pref[i].importance = 0;
+        }
+    }
+        newPrefs = dichotomie(newPrefs.pref, minMax, importab, nbEtapes);
+
+    for (i=0; i<newPrefs.pref.length; i++) {
+        // newImportab = new Array(importab.length).fill(0.0);
+        if (newPrefs.pref[i].importance < importab[importab.length - 1]) { // on remet les niveaux d'importances précédents
+            newPrefs.pref[i].importance = Prefs[i].importance;
+        }
+    }
+
+    console.log("debut rafinement :");
+    console.log(newPrefs.pref);
+
+    newPrefs = rafinement(newPrefs.pref,Prefs, minMax, -1, (importab[importab.length -2]+1));
+    return newPrefs
+}
+
+function recherchePriorite(Prefs,minMax,importab =[0.0,10.0,100.0,1000.0], nbEtapes=100) { // pour chacun des plus importants on se rapproche au maximum
+ // on est pas obligé de faire par dichotomie, on pourrait essayer en diminuant progressivement le critère plus important ça va peut etre plus vite
+    let newImportTab = importab.push(2*importab[importab.length-1]); // on rajoute un degres d'importance
+    let resTab = [];// tableau des noyaux non vide
+
+    for(var i=0; i<Prefs.length; i++){
+        if (Prefs[i].importance == importab[importab.length-2]){ // si c'etait un très important
+            Prefs[i].importance = importab[importab.length-1]; // on met ce critere plus important que tous les autres
+            resTab.push(rechercheUnParUn(Prefs,minMax,newImportTab, nbEtapes=100));
+            Prefs[i].importance = importab[importab.length-1]// on lui rend son importance précédente
+        }
+    }
+    return resTab
+}
+
+
+function rafinement(newPrefs,prefsInit, minMax,importMin=-1, importMax=5,importab =[0,10,100,1000], nbEtapes = 100){
+    i=0;
+    console.log("Debut rafinement :");
+    for (j = 0; j < newPrefs.length; j++){
+        // console.log("test : "+lastPrefsNonVide[j].name+" "+lastPrefsNonVide[j].importance);
+        if ((importMin < prefsInit[j].importance) && (prefsInit[j].importance < importMax)){
+            // si c'etait pas des importants on les traite comme des neutres :
+            newPrefs[j].importance = importab[2];
+            newPrefs[j].pas = prefsInit[j].distVal / newPrefs[j].importance;
+            // var pasId = Prefs[i].name;
+            newPrefs[j].table =[0,0,importab[2]];
+         //   newPrefs[j]=prefsInit[j];
+            console.log(newPrefs[j].table)
+        }
+        console.log("Pref init:");
+        console.log(newPrefs)
+        // si c'etait des importants on garde comme avant
+    }
+    return dichotomie(newPrefs,minMax,importab,nbEtapes,true); // non vide
+}
 
 
 // prend en entree le tableau des pref et le tableau des minMax et le nombres d'étapes max
 // renvoie le tableau correspondant au dernier noyau non vide
 function dichotomie(Prefs, minMax,importab =[0,10,100,1000], nbEtapes = 100, rafinement = false) {
 
-    if (!rafinement){
+    if (!rafinement) {
         for (var i = 0; i < Prefs.length; i++) {
-
-            switch (Prefs[i].importance) {
-                case 'très important':
-                case 'très importante':
-                    //    console.log(Prefs[i].distVal / 1000.0);
-                    Prefs[i].pas = Prefs[i].distVal / parseFloat(importab[3]);
-                    // console.log("pas :" + Prefs[i].pas);
-                    Prefs[i].importance = parseInt(importab[3]);
-                    Prefs[i].table = [0,0,parseInt(importab[3])];
-                    break;
-
-                    //        var pasId = Prefs[i].name;
-                    //       var pas = document.getElementById(pasId);
-                    //       pas.innerHTML = "Augmenter le " + Prefs[i].name + " de " + Prefs[i].pas.toString();
-                    break;
-                case 'important':
-                case 'importante':
-                    Prefs[i].pas = Prefs[i].distVal / parseFloat(importab[2]);
-                    Prefs[i].importance = parseInt(importab[2]);
-                    Prefs[i].table = [0,0,parseInt(importab[2])];
-                    break;
-
-                //         var pasId = Prefs[i].name;
-                //       var pas = document.getElementById(pasId);
-                //     pas.innerHTML = "Augmenter le " + Prefs[i].name + " de " + Prefs[i].pas.toString();
-                case 'neutre':
-                    Prefs[i].pas = Prefs[i].distVal / parseFloat(importab[1]);
-                    // var pasId = Prefs[i].name;
-                    Prefs[i].importance = parseInt(importab[1]);
-                    Prefs[i].table = [0,0,parseInt(importab[1])];
-                    //     var pas = document.getElementById(pasId);
-                    //    pas.innerHTML = "Augmenter le " + Prefs[i].name + " de " + Prefs[i].pas.toString();
-                    break;
-                default :
-                    Prefs[i].importance = parseFloat(importab[0]);
-                    Prefs[i].table = [0,0,parseInt(importab[0])];
-                    break;
+            if (Prefs[i].importance >= 1) {
+                Prefs[i].pas = Prefs[i].distVal / Prefs[i].importance;
             }
-
+            Prefs[i].table = [0, 0, Prefs[i].importance];
             //  console.log(Prefs[i].name + Prefs[i].table);
 
         }
     }
 
+        console.log(Prefs);
 
-    // console.log(Prefs);
     //   debut de l'exploration :
     i=0;
     var lastNonVide = 0;
     var newPrefs = [];
-    var prefsInit = Prefs;
+    var newData = [];
+    var dontstop = false;
+    var prefsInit = JSON.parse(JSON.stringify(Prefs));
     while (i<nbEtapes){
         console.log("******** etape numero: "+i+" ******** "+ "rafinement : "+rafinement);
 
@@ -264,14 +411,24 @@ function dichotomie(Prefs, minMax,importab =[0,10,100,1000], nbEtapes = 100, raf
 
         let AniMin = Prefs[0];
         let AniMax = Prefs[1];
-        let TourMin = Prefs[2];
-        let TourMax = Prefs[3];
-        let CapMin = Prefs[4];
-        let CapMax = Prefs[5];
+        let CapMin = Prefs[2];
+        let CapMax = Prefs[3];
+        let TourMin = Prefs[4];
+        let TourMax = Prefs[5];
         let EnvMin = Prefs[6];
         let EnvMax = Prefs[7];
         let OuvMin = Prefs[8];
         let OuvMax = Prefs[9];
+
+
+        test = TourMin.table[1]*TourMin.signe*TourMin.pas + minMax.TMin;
+        console.log("test");
+        console.log(test);
+        console.log(TourMin.table[1]);
+        console.log(TourMin.signe);
+        console.log(TourMin.pas);
+        console.log(minMax.TMin);
+
         var jsonObj = {
             "room_name": localStorage.getItem("roomName"),
             "class_id": localStorage.getItem("classId"),
@@ -289,45 +446,58 @@ function dichotomie(Prefs, minMax,importab =[0,10,100,1000], nbEtapes = 100, raf
         var data = JSON.stringify(jsonObj);
 
 
+
         console.log(data);
 
         // Dichotomie :
          postXMLHttp('/api?fct=lets_calc' +
             '&data=' + data, function (ret) {
             if (ret == "Ce noyau est vide !"){
+                dontstop = false;
                 console.log("vide");
-                // si la première iteration est vide c'est une erreur : on ne fait rien et on recommence
                 console.log(Prefs);
                 for (var j = 0; j < Prefs.length; j++) {
-                    if(parseInt(Prefs[j].table[1]) === parseInt(Prefs[j].table[0])) {
-                    }else{
-                        console.log(Prefs[j].name + Prefs[j].table);
+
+                     //   console.log(Prefs[j].name + Prefs[j].table);
                         Prefs[j].table[2] = Prefs[j].table[1];
                         Prefs[j].table[1] = Math.floor((Prefs[j].table[0] + Prefs[j].table[1]) / 2.0);
-                        console.log(Prefs[j].name +Prefs[j].table);
-                    }
+                     //   console.log(Prefs[j].name +Prefs[j].table);
+
                 }
             }
             else {
-                console.log("non vide");
-                lastNonVide = nbFile-1;
-                newPrefs = [...Prefs];
-                for (j = 0; j < Prefs.length; j++) {
+                if (ret == "Ce noyau est negatif !") {
+                    // si une iteration est vide c'est une erreur : on ne fait rien et on recommence
+                    console.log("negatif");
+                    dontstop = true;
 
-                    Prefs[j].table[0] = Prefs[j].table[1];
-                    if (Prefs[j].table[1] === Prefs[j].table[0]){ // première iteration
-                        Prefs[j].table[1] = Math.ceil((Prefs[j].table[0] + Prefs[j].table[2]) / 2.0);
+                } else {
+                    dontstop = false;
+                    console.log("non vide");
+                    lastNonVide = nbFile - 1;
+                    newPrefs = JSON.parse(JSON.stringify(Prefs));
+                    newData = data;
+                    console.log(newPrefs);
+                    console.log(Prefs);
 
-                    }else{
-                        Prefs[j].table[1] = Math.ceil((Prefs[j].table[0] + Prefs[j].table[1]) / 2.0);
+                    for (j = 0; j < Prefs.length; j++) {
+                        Prefs[j].table[0] = Prefs[j].table[1];
+                        if (Prefs[j].table[1] === Prefs[j].table[0]) { // première iteration
+                            Prefs[j].table[1] = Math.ceil((Prefs[j].table[0] + Prefs[j].table[2]) / 2.0);
+
+                        } else {
+                            Prefs[j].table[1] = Math.ceil((Prefs[j].table[0] + Prefs[j].table[1]) / 2.0);
+                        }
+
+                        // console.log(Prefs[j].name +Prefs[j].table);
                     }
 
-                    // console.log(Prefs[j].name +Prefs[j].table);
                 }
             }
 
             nbFile = nbFile - 1;
         });
+
         do {
             let tmpPath = "sources/output/" + RoomName + "_" + ClassId + "_" + nbFile + "-viab-0-bound.dat";
             http.open('HEAD', tmpPath, false);
@@ -344,34 +514,23 @@ function dichotomie(Prefs, minMax,importab =[0,10,100,1000], nbEtapes = 100, raf
             }
         }
 
-        if(stop === Prefs.length){
+        if(stop === Prefs.length && (dontstop === false)){
             i=100;
         }
         i++;
     }
 
-    return newPrefs // non vide
+    const result = {
+      pref : newPrefs,
+      data : newData
+    };
 
+    console.log("dernier non vide : ");
+    console.log(result.data);
+    return result ;// non vide
 }
 
-function rafinement(newPrefs,prefsInit, minMax,importab =[0,10,100,1000], nbEtapes = 100){
-    i=0;
-    for (j = 0; j < newPrefs.length; j++){
-        // console.log("test : "+lastPrefsNonVide[j].name+" "+lastPrefsNonVide[j].importance);
-        if (newPrefs[j].importance < importab[1]){
-            // si c'etait pas des importants on les traite comme des neutres
-            prefsInit[j].importance = importab[2];
-            prefsInit[j].pas = prefsInit[j].distVal / prefsInit[j].importance;
-            // var pasId = Prefs[i].name;
-            prefsInit[j].table =[0,0,importab[2]];
-            newPrefs[j]=prefsInit[j]
-        }
-        else{ // si c'etait des importants on garde comme avant
 
-        }
-    }
-    return dichotomie(newPrefs,minMax,importab,nbEtapes,true); // non vide
-}
 
 function sleep(milliseconds) {
     var start = new Date().getTime();
