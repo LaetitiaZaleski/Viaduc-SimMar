1bb0035fcf05707844760fdad6602b73e4a74cf6
varadely <53216427+varadely@users.noreply.github.com>
2020-01-08 11:03:57 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -12,3 +12,20 @@
 // TODO faire un fichier de config (json)
 // TODO déroulement : calcul du noyau par joueur, si non vide --> page résultats --> calcul des intervals, si non vide re-calcul de noyau dans l'interval pour toutes les pref
 // TODO affichage de noyaux a la page result
+
+/*
+POST METHOD
+$.post("https://192.168.1.2:8843/token", {password : "123456"})
+        .done(function(data) {
+            console.log(data);
+        })
+        .fail(function() {
+            console.log("auth KO");
+        })
+        .always(function() {
+            //$('.loading').addClass('d-none');
+        });
+        
+CURL method : 
+curl -X POST --data "password=123456" https://192.168.1.2:8843/token 
+*/
