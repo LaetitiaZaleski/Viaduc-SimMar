c4bb7692bb656eae48c3c60344f01f41c604ad45
varadely <53216427+varadely@users.noreply.github.com>
2020-01-09 14:14:52 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -28,4 +28,53 @@ $.post("https://192.168.1.2:8843/token", {password : "123456"})
         
 CURL method : 
 curl -X POST --data "password=123456" https://192.168.1.2:8843/token 
+
+curl -X POST -F 'script=Pi.oms' -F 'workDirectory=@pi.tgz' http://localhost:8843/job
+--port 8843
+
+// GO 
+func SendPostRequest (url string, filename string, filetype string) []byte {
+    file, err := os.Open(filename)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+
+    body := &bytes.Buffer{}
+    writer := multipart.NewWriter(body)
+    part, err := writer.CreateFormFile(filetype, filepath.Base(file.Name()))
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    io.Copy(part, file)
+    writer.Close()
+    request, err := http.NewRequest("POST", url, body)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    request.Header.Add("Content-Type", writer.FormDataContentType())
+    client := &http.Client{}
+
+    response, err := client.Do(request)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer response.Body.Close()
+
+    content, err := ioutil.ReadAll(response.Body)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return content
+}
+
 */
